4d2710b9c9a31ed384373326e4a06d831f71a438
rushikeshtonape <rushikeshtonape143@gmail.com>
2023-03-30 16:51:13 +0530

    Add form into AddUser.js

--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -1,7 +1,56 @@
 import React from "react";
 
 const AddUser = () => {
-  return <div>AddUser form</div>;
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 border rounded offset-md-3  mt-2 p-4 shadow">
+            <h2 className="text-center m-6">Register here</h2>
+            <div className="mb-3">
+              <label htmlFor="name" className="m-2">
+                Name:
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                name="name"
+                placeholder="Enter the Name"
+              />
+            </div>
+
+            <div className="mb-3">
+              <label htmlFor="username" className="m-2">
+                UserName:
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                name="username"
+                placeholder="Enter the UserName"
+              />
+            </div>
+
+            <div className="mb-3">
+              <label htmlFor="email" className="m-2">
+                Email:
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                name="email"
+                placeholder="Enter the Email"
+              />
+            </div>
+            <button className="btn btn-primary" type="submit">
+              Submit
+            </button>
+            <button className="btn btn-outline-danger m-2">Cancel</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default AddUser;
